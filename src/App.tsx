b0203081463b42593ccc666c2ffcace6ffe6a131
Pier Daniele Cretara <pierdanielecretara@lviiier.com>
2022-03-30 23:24:09 +0200

    Update to React 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Test new React Features, updated to 18</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
